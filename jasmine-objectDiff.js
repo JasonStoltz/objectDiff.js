d5098327d5f002103bdd1c7005034d1bfa420823
JasonStoltz <jastoltz@gmail.com>
2015-02-04 11:05:24 -0500

    slimming down functionality and publishing to bower

--- jasmine-objectDiff.js
+++ jasmine-objectDiff.js
@@ -3,23 +3,8 @@ objectDiff.jasmine = {
 	toEqualProperties: function(expected) {
 		var diff = objectDiff.diff(expected, this.actual);
 		this.message = function() {
-			var pre = document.createElement('pre');
-			pre.className = 'diff-holder';
-			pre.innerHTML = objectDiff.convertToXMLString(diff);
-			return pre;
-		};
-		return diff.changed === 'equal';
-	},
-
-	toEqualOwnProperties: function(expected) {
-		var diff = objectDiff.diffOwnProperties(expected, this.actual);
-		this.message = function() {
-			var pre = document.createElement('pre');
-			pre.className = 'diff-holder';
-			pre.innerHTML = objectDiff.convertToXMLString(diff);
-			return pre;
+			return JSON.stringify(diff);
 		};
 		return diff.changed === 'equal';
 	}
-
 };
