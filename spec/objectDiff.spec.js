66bffcb56105e7b6b5d8a1c123290bbb1576f65e
JasonStoltz <jastoltz@gmail.com>
2015-02-04 11:05:24 -0500

    slimming down functionality and publishing to bower

--- spec/objectDiff.spec.js
+++ spec/objectDiff.spec.js
@@ -12,33 +12,15 @@ describe('toEqualProperties', function() {
 			y: 3
 		};
 
-		var b = new Mock;
-		b.message = 'bar';
-		delete b.size;
-		b.point = {
-			x: 2,
-			y: 7
-		};
+		var b = {
+			message: 'foo',
+			size: 1,
+			point: {
+				x: 2,
+				y: 3
+			}
+		}
 
 		expect(a).toEqualProperties(b);
-	});
-});
-
-describe('toEqualOwnProperties', function() {
-	it('shows diff', function() {
-		var a = new Mock;
-		a.point = {
-			x: 2,
-			y: 3
-		};
-
-		var b = new Mock;
-		b.message = 'bar';
-		b.point = {
-			x: 2,
-			y: 42
-		};
-
-		expect(a).toEqualOwnProperties(b);
 	});
 });
